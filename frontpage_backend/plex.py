29ace84f280a8f1f24a8fe68ac13395d0d445c8c
Joacim Eriksson <joacim@cyriusg.se>
2017-01-29 21:20:26 +0100

    Forgot adding the plex file.

---
+++ frontpage_backend/plex.py
@@ -0,0 +1,64 @@
+import json, pycurl
+from io import BytesIO
+from bs4 import BeautifulSoup
+
+class Plex():
+
+    def __init__(self, host, port):
+        self.__host = host
+        self.__port = port
+        self.reply = {}
+
+    def __apicall(self, host, port, endpoint):
+        buffer = BytesIO()
+        c = pycurl.Curl()
+
+        header = [
+            'Content-Type: application/xml; charset=utf-8',
+            'Content-Length: 0',
+            'X-Plex-Product: YTB-SSO',
+            'X-Plex-Version: v2.0',
+        ]
+
+        url = 'http://' + host + ':' + port + endpoint
+
+        c.setopt(pycurl.URL, url)
+        c.setopt(pycurl.HTTPHEADER, header)
+
+        c.setopt(pycurl.TIMEOUT, 10)
+        c.setopt(c.WRITEDATA, buffer)
+
+        try:
+            c.perform()
+            c.close()
+        except pycurl.error:
+            return 'Failed communicating with Plex.'
+
+        try:
+            return buffer.getvalue().decode('utf-8')
+        except json.JSONDecodeError:
+            return 'JSON API failure'
+
+    def search_for_movie(self, title, year):
+        request = self.__apicall(self.__host, self.__port, '/search?query=' + title.replace(' ', '%20'))
+
+        body = BeautifulSoup(request, 'html.parser')
+        match = False
+
+        for video in body.mediacontainer.findAll('video'):
+            if video['title'] == title and video['year'] == year and video['type'] == 'movie':
+                match = True
+
+        return match
+
+    def search_for_show(self, title, year):
+        request = self.__apicall(self.__host, self.__port, '/search?query=' + title.replace(' ', '%20'))
+
+        body = BeautifulSoup(request, 'html.parser')
+        match = False
+
+        for video in body.mediacontainer.findAll('video'):
+            if video['title'] == title and video['year'] == year and video['type'] == 'show':
+                match = True
+
+        return match
